c0eecf1277219c9070ea34b1b6fdcc1d5b057682
Justus Witmer <justus.witmer@gmail.com>
2020-10-28 14:38:26 -0500

    installed all the things

---
+++ src/Owner/Owner.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Owner extends Component {
+  render() {
+    return (
+      <div>
+        In Owner
+      </div>
+    )
+  }
+}
+
+export default Owner;
