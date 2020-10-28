2439e4b0e05dd4052a4ec8e402ce206dcfea2b1d
leahbrenny <66805316+leahbrenny@users.noreply.github.com>
2020-10-28 10:28:46 -0500

    added sql code

---
+++ database.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "owner" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (80) NOT NULL,
+);
+
+CREATE TABLE "pet" (
+    "id" SERIAL PRIMARY KEY,
+    "owner_id" INT NOT NULL,
+    "name" VARCHAR (80) NOT NULL,
+    "breed" VARCHAR (80) NOT NULL,
+    "color" VARCHAR (20) NOT NULL,
+    "checked_in" DATE
+);
