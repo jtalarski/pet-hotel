2bd741f283060d7db7ffc06ec2be36cf0b619e4e
leahbrenny <66805316+leahbrenny@users.noreply.github.com>
2020-10-29 10:30:51 -0500

    updated root reducer and saga to include pet

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 // import myReducer from './myReducer.reducer';
 import owner, { saveOwnerReducer } from './owner.reducer';
+import pets from './pet.reducer';
 
 const rootReducer = combineReducers({
   // list of reducers goes here
   // example: myReducer,
   owner,
   saveOwnerReducer,
+  pets,
 });
 
 export default rootReducer;
--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -1,11 +1,13 @@
 import { all } from 'redux-saga/effects';
 //import mySaga from './mySaga.saga';
 import ownerSaga from './owner.saga';
+import petSaga from './pet.saga';
 
 export default function* rootSaga() {
   yield all([
     // list of sagas goes here
     // example: mySaga(),
     ownerSaga(),
+    petSaga(),
   ]);
 }
